074ba717c7cb2079dc72ecab063b005bfb90ce2d
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-08-20 15:41:52 +1000

    Update test framework for separate bool and int. Added switch tester

--- c_version/timer_test.c
+++ c_version/timer_test.c
@@ -8,23 +8,60 @@
 #define GREEN   "\x1B[32m"
 #define RESET   "\x1B[0m"
 
+#define MAX_STRING 100
+
 int tests_run = 0;
 int expect;
 int result;
+bool resultBool;
+bool expectBool;
 
-void assertStats(int expect, int result);
+void assertInt(int expect, int result);
+void assertBool(bool expect, bool result);
+
+static char * correctQuietSwitch() {
+     printf("Correct quiet switch entered...\t\t\t\t\t\t");
+     char* switchEnd = "--quiet";
+
+     bool quietMode = false;
+     bool dryRun = false;
+     checkforEndSwitch(switchEnd, &quietMode, &dryRun);
+
+     expectBool = true;
+     resultBool = quietMode;
+     assertBool(expectBool, resultBool);
+     mu_assert("", resultBool == expectBool);
+     return 0;
+ }
+
+static char * correctDrySwitch() {
+     printf("Correct dry switch entered...\t\t\t\t\t\t");
+     char* switchEnd = "--dry-run";
+
+     bool quietMode = false;
+     bool dryRun = false;
+     checkforEndSwitch(switchEnd, &quietMode, &dryRun);
+
+     expectBool = true;
+     resultBool = dryRun;
+     assertBool(expectBool, resultBool);
+     mu_assert("", resultBool == expectBool);
+     return 0;
+ }
 
  static char * hourMinsSeconds() {
      printf("Convert hours, mins, seconds parameters to just seconds...\t\t");
      expect = 10921;
      result = convertHoursMinsToSecondsInt(3, 2, 1);
-     assertStats(expect, result);
+     assertInt(expect, result);
      mu_assert("", result == expect);
      return 0;
  }
 
  static char * all_tests() {
      mu_run_test(hourMinsSeconds);
+     mu_run_test(correctQuietSwitch);
+     mu_run_test(correctDrySwitch);
      return 0;
  }
 
@@ -42,7 +79,7 @@ void assertStats(int expect, int result);
      return result != 0;
  }
 
-void assertStats(int expect, int result){
+void assertInt(int expect, int result){
      if (result != expect){
          printf(RED "[ FAIL] expected %d: ", expect);
          printf("got %d\n", result);
@@ -50,5 +87,15 @@ void assertStats(int expect, int result){
      }else
         printf(GREEN "[ PASS ]\n");
          printf(RESET);
-
 }
+
+void assertBool(bool expect, bool result){
+     if (result != expect){
+         printf(RED "[ FAIL] expected %s: ", expect ? "true" : "false");
+         printf("got %s\n", result ? "true": "false");
+         printf(RESET);
+     }else
+        printf(GREEN "[ PASS ]\n");
+         printf(RESET);
+}
+

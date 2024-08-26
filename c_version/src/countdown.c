52ecc2362dca7fae472cbda32c1b63f40349ef11
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-08-26 13:18:04 +1000

    Added countdown.c file

---
+++ c_version/src/countdown.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
+#include "../include/timer.h"
+
+void secondsCountdown (int seconds){
+   struct tm *endTimeInfo;
+   time_t endTime = time(NULL) + seconds;
+   printf("\n");
+
+   int timeDifference =  endTime - time(NULL);
+
+   while(timeDifference > 1){
+       timeDifference =  endTime - time(NULL);
+       int h = seconds / 3600;
+       int m = (seconds  % 3600) / 60;
+       int s = seconds  % 60;
+
+       printf ("\r%02d:%02d:%02d", h, m, s);
+       fflush(stdout);
+       sleep(1);
+       seconds--;
+   }
+   printf("\nTime's up!\n");
+}
+

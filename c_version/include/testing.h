54bf118f223088d222720604caaddf59640c3f8d
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-08-27 15:49:29 +1000

    Merged minunit tests into testing.h for easier use

--- c_version/include/testing.h
+++ c_version/include/testing.h
@@ -1,6 +1,14 @@
+// https://jera.com/techinfo/jtns/jtn002
+#define mu_assert(message, test) \
+    do { if (!(test)) return message; } while (0)
+#define mu_run_test(test) do { char *message = test(); tests_run++; \
+                               if (message) return message; } while (0)
+
+extern int tests_run;
 #ifndef FILE_HANDLING_H
 #define FILE_HANDLING_H
 #endif
 
 void assertString(char* expect, char* result);
 void assertInt(int expect, int result);
+

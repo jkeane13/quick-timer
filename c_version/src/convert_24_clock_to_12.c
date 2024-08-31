612fff3360a654b76744f503023d2b665c81118c
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-08-31 14:10:09 +1000

    separate conversion clocks into its own separate file

---
+++ c_version/src/convert_24_clock_to_12.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "../include/timer.h"
+
+void convert24ClockTo12(char* clockTime){
+    char timerSuffix[4];
+
+    int lastCharPostition = strlen(clockTime) - 1;
+    int secondLastCharPostition = strlen(clockTime) - 2;
+
+    char hourString[] = {clockTime[0], clockTime[1],'\0'};
+    char minString[] = {clockTime[secondLastCharPostition], clockTime[lastCharPostition],'\0'};
+
+    char separator = ':';
+    int hour;
+    int minute;;
+
+    hour = atoi(hourString);
+
+    if (hour < 12)
+        strcpy(timerSuffix,"am");
+    else{
+        hour -= 12;
+        strcpy(timerSuffix,"pm");
+    }
+
+    if (hour == 0)
+        hour +=12;
+
+    sprintf(hourString, "%d", hour);
+    strcpy(clockTime, hourString);
+    minute = atoi(minString);
+
+    if (minute > 0){
+        strncat(clockTime, &separator,1);
+        strcat(clockTime, minString);
+    }
+
+    strcat(clockTime, timerSuffix);
+}

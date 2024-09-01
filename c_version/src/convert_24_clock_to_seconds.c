984c5f508132e8ae5d84563f3989737e521aa92f
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-09-01 13:41:13 +1000

    Moved more files around to reduce code line size

--- c_version/src/convert_24_clock_to_seconds.c
+++ c_version/src/convert_24_clock_to_seconds.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include <time.h>
 #include "../include/timer.h"
-
-#define UNIX_NULL_OUTPUT " >/dev/null 2>&1"
 
 int convert24ClockToSeconds(char* input){
     if (strlen(input) > 5 || input[2] != ':'){
@@ -53,31 +50,9 @@ void convertIntToDoubleString(int number, char stringNumber[]){
     }
 }
 
-void printSecondsEndAsClock(int seconds, char* clockType){
-   struct tm *endTimeInfo;
-   time_t endTime = time(NULL) + seconds;
+int convertHoursMinsToSeconds(int hours, int minutes, int seconds){
+    hours = hours * 60 * 60;
+    minutes = minutes * 60;
 
-   endTimeInfo = localtime(&endTime);
-
-   char minString[3];
-   char hourString[3];
-   char outputString[10];
-
-   convertIntToDoubleString(endTimeInfo->tm_hour, hourString);
-   convertIntToDoubleString(endTimeInfo->tm_min, minString);
-
-   strcpy(outputString,hourString);
-   strcat(outputString,":");
-   strcat(outputString,minString);
-
-   if (strcmp(clockType,"24Hour") != 0 )
-       convert24ClockTo12(outputString);
-
-   printf("%s\n", outputString);
+    return seconds = hours + minutes + seconds;
 }
-
-void printTimerEndTime (int seconds){
-   printf("Timer ends at ");
-   printSecondsEndAsClock(seconds,"12Hour");
-}
-
--- c_version/src/main.c
+++ c_version/src/main.c
@@ -5,8 +5,6 @@
 #define ALERT_TIMES 1
 #define DISPLAY_OUTPUT 0
 
-void usage();
-
 int main(int argc, char *argv[]){
     int quietMode = 0;
     int dryRunMode = 0;
@@ -44,32 +42,3 @@ int main(int argc, char *argv[]){
         runProgram(argSwitch, DISPLAY_OUTPUT);
 }
 
-void usage(void){
-    printf("usage:\n"
-           "Countdown to the second, minute or hour,  or by a clock time\n"
-           "                                                            \n"
-           "timer [seconds]                                             \n"
-           "timer [minutes] [seconds]                                   \n"
-           "timer [hours] [minutes] [seconds]                           \n"
-           "timer [clockAM/PM]                                          \n"
-           "timer -p                                                    \n"
-           "                                                            \n"
-           "Switches                                                    \n"
-           "-p              prompt time input                           \n"
-           "--quiet         Doesn't play the alert                      \n"
-           "--dry-run         Doesn't play the alert                    \n"
-           "                                                            \n"
-           "EXAMPLE: 2 hours, 10 minutes, 1 second                      \n"
-           "timer 2 10 1                                                \n"
-           "                                                            \n"
-           "EXAMPLE: Timer for 5:30 24 hours                            \n"
-           "timer 5:30                                                  \n"
-           "                                                            \n"
-           "EXAMPLE: Timer for 5:30am 12 hour clock                     \n"
-           "timer 5:30am                                                \n"
-           "                                                            \n"
-           "EXAMPLE: Timer for 5pm 12 hour clock                        \n"
-           "timer 5pm                                                   \n"
-           "                                                            \n"
-           );
-}

f557b1e3cf6d2dff4b2b5eec02a3ac55bc53d04e
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-09-06 20:23:29 +1000

    Updated argument file name to make it more efficient

--- c_version/src/args_switch_handler.c
+++ c_version/src/args_switch_handler.c
@@ -1,17 +1,14 @@
 #include <string.h>
 #include "../include/timer.h"
 
-void runEndSwitch(char* argument, 
-                  int *quietMode,
-                  int *dryRunMode,
-                  int *executeMode)
+void runEndSwitch(char* arg, int *quietMode, int *dryRunMode, int *executeMode)
 {
-    if (strcmp(argument, "--quiet") == 0){
+    if (strcmp(arg, "--quiet") == 0){
         *quietMode = 1;
-    } else if (strcmp(argument, "--dry-run") == 0){
+    } else if (strcmp(arg, "--dry-run") == 0){
         *quietMode = 1;
         *dryRunMode = 1;
-    } else if (strstr(argument, ".") != 0){
+    } else if (strstr(arg, ".") != 0){
         *quietMode = 1;
         *executeMode = 1;
     }

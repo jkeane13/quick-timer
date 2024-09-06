937bef4deac484e0cfbbce3a2565222e326c5769
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-09-06 20:24:16 +1000

    Removed double int string function with sprintf

--- c_version/src/convert_24_clock_to_seconds.c
+++ c_version/src/convert_24_clock_to_seconds.c
@@ -10,15 +10,13 @@ int convert24ClockToSeconds(char* input){
         printf("Format needs to be 24 hour time - 00:00\n");
         exit(1);
     }
-    char hourInput[] = {input[0], input[1], '\0'};
-    char minInput[] = {input[3], input[4], '\0'};
     struct tm *nowTime;
     time_t currentTime = time(NULL);
 
     nowTime = localtime(&currentTime);
 
-    int hourInt = atoi(hourInput);
-    int minuteInt = atoi(minInput);
+    int hourInt = getHour(input);
+    int minuteInt = getMinutes(input);
     int seconds = 0;
 
     if (nowTime->tm_hour > hourInt)
@@ -32,22 +30,9 @@ int convert24ClockToSeconds(char* input){
     }
 
     int hourDifference =  hourInt - nowTime->tm_hour;
-
-    seconds = convertHoursMinsToSeconds(hourDifference,
-                                        minDifference,
-                                        0);
+    seconds = convertHoursMinsToSeconds(hourDifference, minDifference, 0);
 
     return seconds;
-}
-
-void convertIntToDoubleString(int number, char stringNumber[]){
-    if (number < 10){
-        stringNumber[0] = '0';
-        stringNumber[1] = number + '0';
-        stringNumber[2] = '\0';
-    } else{
-        sprintf(stringNumber, "%d", number);
-    }
 }
 
 int convertHoursMinsToSeconds(int hours, int minutes, int seconds){

adefa42be7a4f254d5a189e5464d9fee953136ad
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-09-06 20:25:03 +1000

    Removed double int tests

--- c_version/tests/convert_24_hour_clock_to_seconds_test.c
+++ c_version/tests/convert_24_hour_clock_to_seconds_test.c
@@ -13,33 +13,6 @@ char inputString[MAX_STRING];
 char resultString[MAX_STRING];
 char expectString[MAX_STRING];
 
-// convertIntToDoubleString
-static char * convertIntToDoublestring08() {
-    printf("Convert integer 8 to double string \"08\"\t\t\t\t\t");
-    strcpy(inputString,"9");
-    convertIntToDoubleString(9,inputString);
-    strcpy(expectString,"09");
-
-    strcpy(resultString, inputString);
-    assertString(expectString, resultString);
-
-    mu_assert("", result == expect);
-    return 0;
-}
-
-static char * convertIntToDoublestring12() {
-    printf("Convert integer 12 to double string \"12\"\t\t\t\t");
-    strcpy(inputString,"12");
-    convertIntToDoubleString(12,inputString);
-    strcpy(expectString,"12");
-
-    strcpy(resultString, inputString);
-    assertString(expectString, resultString);
-
-    mu_assert("", result == expect);
-    return 0;
-}
-
 static char * convert321HoursMinsToSeconds(){
     printf("Convert 3 Hours, 2 mins and 1 second to seconds\t\t\t\t");
     result = convertHoursMinsToSeconds(3,2,1);
@@ -52,8 +25,6 @@ static char * convert321HoursMinsToSeconds(){
 }
 
 static char * all_tests() {
-    mu_run_test(convertIntToDoublestring08);
-    mu_run_test(convertIntToDoublestring12);
     mu_run_test(convert321HoursMinsToSeconds);
     return 0;
 }
@@ -64,5 +35,4 @@ int main(int argc, char **argv) {
 
     return result != 0;
 }
-// convert24ClockToSeconds
 

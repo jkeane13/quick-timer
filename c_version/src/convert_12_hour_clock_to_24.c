b9058c7dd41c0b61e14f11bd40a46241adab717e
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-09-06 16:59:33 +1000

    Separated conversion types lsinto separate functions

--- c_version/src/convert_12_hour_clock_to_24.c
+++ c_version/src/convert_12_hour_clock_to_24.c
@@ -5,22 +5,11 @@
 
 void addMinutesToClock(char* clock24Time, char* timeInput);
 
-void convert12to24(char* timeInput){
+void convertSingleNumberAmPmTimeto24(char* clock24Time, char *timeInput){
     int timeInputLength = strlen(timeInput);
-    char pmString[3];
-
-    char clock24Time[6];
     char lastChar = timeInput[timeInputLength-1];
     char secondLastChar = timeInput[timeInputLength-2];
-    char thirdLastChar = timeInput[timeInputLength-3];
-
-    // *:** and **.**
-    if (thirdLastChar == ':'){
-        convertQuickClockto24Hour(timeInput);
-        strcpy(clock24Time,timeInput);
-    }
-
-    // * am and pm
+    char pmString[3];
     if (timeInputLength == 3 & lastChar == 'm'){
         if (secondLastChar == 'p'){
             int hourNumber = timeInput[0] - '0';
@@ -33,8 +22,13 @@ void convert12to24(char* timeInput){
         }
         strcat(clock24Time, ":00");
     }
+}
 
-    // ** am and pm
+void convertDoubleNumberAmPmTimeto24(char* clock24Time,char *timeInput){
+    int timeInputLength = strlen(timeInput);
+    char lastChar = timeInput[timeInputLength-1];
+    char secondLastChar = timeInput[timeInputLength-2];
+    char pmString[3];
     if (timeInputLength == 4 & lastChar == 'm'){
         int addHours = 0;
         if (secondLastChar == 'p')
@@ -51,7 +45,12 @@ void convert12to24(char* timeInput){
         strcat(clock24Time, ":00");
     }
 
-    // *:** am and pm
+}
+void convertSingleHourMinuteAmPmTimeto24(char* clock24Time,char *timeInput){
+    int timeInputLength = strlen(timeInput);
+    char lastChar = timeInput[timeInputLength-1];
+    char secondLastChar = timeInput[timeInputLength-2];
+    char pmString[3];
     if (timeInputLength == 6 & lastChar == 'm'){
         if (secondLastChar == 'p'){
             int hourNumber = timeInput[0] - '0';
@@ -67,8 +66,12 @@ void convert12to24(char* timeInput){
         clock24Time[4] = timeInput[3];
         clock24Time[5] =  '\0';
     }
-
-    // **:** am and pm
+}
+void convertDoubleHourMinuteAmPmTimeto24(char *clock24Time,char *timeInput){
+    int timeInputLength = strlen(timeInput);
+    char lastChar = timeInput[timeInputLength-1];
+    char secondLastChar = timeInput[timeInputLength-2];
+    char pmString[3];
     if (timeInputLength == 7){
         clock24Time[0] = timeInput[0];
         clock24Time[1] = timeInput[1];
@@ -85,7 +88,34 @@ void convert12to24(char* timeInput){
 
         addMinutesToClock(clock24Time, timeInput);
     }
-    strcpy(timeInput,clock24Time);
+}
+
+void convertQuickClockEntryTo24(char* clock24Time,char* timeInput) {
+    int timeInputLength = strlen(timeInput);
+    char thirdLastChar = timeInput[timeInputLength-3];
+    if (thirdLastChar == ':')
+        convertQuickClockto24Hour(timeInput);
+}
+
+void convert12to24(char* timeInput){
+    int timeInputLength = strlen(timeInput);
+
+    char clock24Time[6];
+    char thirdLastChar = timeInput[timeInputLength-3];
+
+    convertQuickClockEntryTo24(clock24Time,timeInput);
+
+    // * am and pm
+    convertSingleNumberAmPmTimeto24(clock24Time, timeInput);
+
+    // ** am and pm
+    convertDoubleNumberAmPmTimeto24(clock24Time,timeInput);
+
+    // *:** am and pm
+    convertSingleHourMinuteAmPmTimeto24(clock24Time,timeInput);
+
+    // **:** am and pm
+    convertDoubleHourMinuteAmPmTimeto24(clock24Time,timeInput);
 }
 
 void addMinutesToClock(char* clock24Time, char* timeInput){

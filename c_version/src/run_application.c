f30fc8e8780d1e5caa8decf2f741cc087f7e0da4
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-09-06 12:51:55 +1000

    Simplified Run Application to use sprintf

--- c_version/src/run_application.c
+++ c_version/src/run_application.c
@@ -12,14 +12,11 @@
 #define WIN_PLAYER "mpg123"
 #define LINUX_PLAYER "mpg123"
 #define MAX_COMMAND_LENGTH 100
-#define UNIX_NULL_OUTPUT " >/dev/null 2>&1"
-#define WIN_NULL_OUTPUT " >NUL 2>&1"
+#define UNIX_NULL_OUTPUT ">/dev/null 2>&1"
+#define WIN_NULL_OUTPUT ">NUL 2>&1"
 
 void playSound(int times){
     char soundCommand[MAX_COMMAND_LENGTH];
-    char homeFolder[MAX_COMMAND_LENGTH];
-    char unixSeparator = '/';
-    char winSeparator = '\\';
 
     #if defined(__APPLE__)
         strcpy (soundCommand, MAC_PLAYER);
@@ -27,20 +24,13 @@ void playSound(int times){
     #if defined(__linux__)
         strcpy (soundCommand, LINUX_PLAYER);
     #endif
-
     #if defined(__linux__) || defined(__APPLE__)
-        strcat (soundCommand," ");
-        strcat (soundCommand, HOME_FOLDER_UNIX);
-        strncat (soundCommand, &unixSeparator, 1);
-        strcat (soundCommand,UNIX_FILENAME);
-        strcat (soundCommand,UNIX_NULL_OUTPUT);
+        sprintf(soundCommand,"%s %s/%s %s",soundCommand,HOME_FOLDER_UNIX,
+                                           UNIX_FILENAME,UNIX_NULL_OUTPUT);
     #elif defined(_WIN32)
-        strcpy (soundCommand, WIN_PLAYER);
-        strcat (soundCommand," ");
-        strcat (soundCommand, HOME_FOLDER_WINDOWS);
-        strncat (soundCommand, &winSeparator, 1);
-        strcat (soundCommand,WIN_FILENAME);
-        strcat (soundCommand,WIN_NULL_OUTPUT);
+        sprintf(soundCommand,"%s %s\\%s %s",soundCommand,WIN_PLAYER,
+                                           HOME_FOLDER_WINDOWS,WIN_FILENAME,
+                                           WIN_NULL_OUTPUT);
     #endif
 
     for (int i = 0; i < times; i++)

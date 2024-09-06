ad34583f0051f50bb8b1f8f62b4ef3f134a1c1b7
Jason Keane <jason.keane@davidjones.com.au>
2024-09-06 22:44:23 +1000

    Fixed issue minute being added with minute entry was blank

--- c_version/src/get_hour_and_minutes.c
+++ c_version/src/get_hour_and_minutes.c
@@ -7,22 +7,23 @@ int getHour(char* timeInput){
     char hourString[2];
     for(int i = 0; i< strlen(timeInput); i++)
         if (isdigit(timeInput[i]))
-           hourString[i] = timeInput[i]; 
+           hourString[i] = timeInput[i];
 
     return atoi(hourString);
 }
 
 int getMinutes(char* timeInput){
-    char minString[2];
-    for(int i = 0; i< strlen(timeInput); i++)
-        if (timeInput[i] == ':'){
-           minString[0] = timeInput[i+1]; 
-           minString[1] = timeInput[i+2]; 
-        }
-
-    if (strlen(minString) == 0)
+    if (strchr(timeInput,':') == 0)
         return 0;
-    else
-        return atoi(minString);
+
+    char minString[3];
+    for(int i = 0; i< strlen(timeInput); i++){
+        if (timeInput[i] == ':'){
+           minString[0] = timeInput[i+1];
+           minString[1] = timeInput[i+2];
+           minString[2] = '\0';
+        }
+    }
+    return atoi(minString);
 }
 

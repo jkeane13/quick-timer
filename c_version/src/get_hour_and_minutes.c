77e6fe157173418c0ab01554a42f3443b77fafcd
Jason Keane <19480056+jkeane13@users.noreply.github.com>
2024-09-08 12:35:06 +1000

    Added null terminator on hour and minute string for safety

--- c_version/src/get_hour_and_minutes.c
+++ c_version/src/get_hour_and_minutes.c
@@ -1,29 +1,35 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include "../include/timer.h"
 
 int getHour(char* timeInput){
-    char hourString[2];
-    for(int i = 0; i< strlen(timeInput); i++)
-        if (isdigit(timeInput[i]))
-           hourString[i] = timeInput[i];
+   char hourString[3];
+   for(int i = 0; i< strlen(timeInput); i++)
+       if (isdigit(timeInput[i]))
+          hourString[i] = timeInput[i];
+       else{
+           hourString[i] = '\0';
+       }
+   printf("Hour String %s\n",hourString);
 
-    return atoi(hourString);
+   return atoi(hourString);
 }
 
 int getMinutes(char* timeInput){
-    if (strchr(timeInput,':') == 0)
-        return 0;
+   if (strchr(timeInput,':') == 0)
+       return 0;
 
-    char minString[3];
-    for(int i = 0; i< strlen(timeInput); i++){
-        if (timeInput[i] == ':'){
-           minString[0] = timeInput[i+1];
-           minString[1] = timeInput[i+2];
-           minString[2] = '\0';
-        }
-    }
-    return atoi(minString);
+   char minString[3];
+   for(int i = 0; i< strlen(timeInput); i++){
+       if (timeInput[i] == ':'){
+          minString[0] = timeInput[i+1];
+          minString[1] = timeInput[i+2];
+          minString[2] = '\0';
+       }
+   }
+   printf("Minutes String %s\n",minString);
+   return atoi(minString);
 }
 

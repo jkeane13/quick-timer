4ef9d363d9964517eece1e8332a0353bbc921dc6
Jason Keane <jason.keane@davidjones.com.au>
2024-09-15 13:55:49 +1000

    Removed play sound and replace characters to simplify c file

--- src/run_application.c
+++ src/run_application.c
@@ -4,52 +4,9 @@
 #include <string.h>
 #include "../include/timer.h"
 
-#define AF_PLAYER "afplay"
-#define MPG_PLAYER "mpg123"
 #define MAX_COMMAND_LENGTH 1024
 #define WINDOWS_NULL_OUTPUT " > nul 2>&1"
 #define UNIX_NULL_OUTPUT " >/dev/null 2>&1"
-
-void replaceChar(char* str, char find, char replace){
-    char *current_pos = strchr(str,find);
-    while (current_pos) {
-        *current_pos = replace;
-        current_pos = strchr(current_pos,find);
-    }
-}
-
-void removeChar(char *str, char keyCharacter) {
-    char *src, *dst;
-    for (src = dst = str; *src != '\0'; src++) {
-        *dst = *src;
-        if (*dst != keyCharacter) dst++;
-    }
-    *dst = '\0';
-}
-
-void playSound(char *soundFile, int times){
-    char soundCommand[MAX_COMMAND_LENGTH];
-
-    #if defined(__APPLE__)
-        strcpy (soundCommand, AF_PLAYER);
-    #endif
-    #if defined(__linux__) || (__WIN32)
-        strcpy (soundCommand, MPG_PLAYER);
-    #endif
-
-    strcat (soundCommand, " ");
-    strcat(soundCommand,soundFile);
-
-    #if defined(__linux__) || defined(__APPLE__)
-        strcat(soundCommand,UNIX_NULL_OUTPUT);
-    #elif defined(_WIN32)
-        strcat(soundCommand,WINDOWS_NULL_OUTPUT);
-        replaceChar(soundCommand, '/', '\\');
-    #endif
-
-    for (int i = 0; i < times; i++)
-        system(soundCommand);
-}
 
 void checkForHomeFolderPath(char* filePath){
     if (filePath[0] == '~'){
@@ -65,7 +22,6 @@ void checkForHomeFolderPath(char* filePath){
         removeChar(fullPath,'~');
         strcpy(filePath,fullPath);
     }
-
 }
 
 void checkFileExists(char* programLocation){

aef0b415d5dcecbd786a01d176758db28fe44fc5
Jason Keane <jason@random.com.au>
2026-01-10 10:44:25 +1100

    fixed windows compiler to use a wildcard and copying config to the right home drive

--- windows_compile/install.bat
+++ windows_compile/install.bat
@@ -5,31 +5,30 @@ IF NOT EXIST "C:\Program Files\Quick Timer\" (
     MKDIR "C:\Program Files\Quick Timer"
 )
 
-IF NOT EXIST %homepath%\.local (
+IF NOT EXIST %homedrive%%homepath%\.local (
     ECHO Creating .local folder in home folder...
     MKDIR %homepath%\.local
 )
 
-IF NOT EXIST %homepath%\.local\assets (
+IF NOT EXIST %homedrive%%homepath%\.local\assets (
     ECHO Create assets folder in .local folder...
     MKDIR %homepath%\.local\assets
 )
 
-IF NOT EXIST %homepath%\.local\config (
+IF NOT EXIST %homedrive%%homepath%\.local\config (
     ECHO Create configuration folder in .config...
-    MKDIR %homepath%\.local\config
+    MKDIR %homedrive%%homepath%\.local\config
 )
 
 ECHO Copying Timer...
 COPY ..\build\timer.exe "C:\Program Files\Quick Timer\"
 
 ECHO Copying Timer Alert...
-COPY ..\assets\stopwatch.mp3 %homepath%\.local\assets\
+COPY ..\assets\stopwatch.mp3 %homedrive%%homepath%\.local\assets\
 
 ECHO Copy Configuration...
-COPY ..\config\timer.cfg %homepath%\.local\config\
+COPY ..\config\timer.cfg %homedrive%%homepath%\.local\config\
 
 IF NOT EXIST "C:\Program Files\Quick Timer\mpg123.exe" (
     ECHO "Alarm mpg123.exe needs to be put in this directory, for the alarm to work."
 )
-

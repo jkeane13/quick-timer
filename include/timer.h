1c14bc1af07fff93292d66cf69ff2cb4f37e48ec
Jason Keane <jason@random.com.au>
2026-06-10 22:10:10 +1000

    Refactored mode switch

--- include/timer.h
+++ include/timer.h
@@ -9,14 +9,18 @@
 #define TEST_CHARACTER '?'
 #define RANDOM_TEST_VALUE 42
 
+#define QUIET_MODE_SWITCH "--quiet"
+#define DRY_RUN_MODE_SWITCH "--dry-run"
+#define FILE_DIVIDER '.'
+
 int t_check_argument(char* input);
 int t_prompt_time_input(int seconds);
+void t_set_mode_switch(const char* arg, int *quiet_mode, int *dry_run_mode, int *execute_mode);
 int convertToSeconds(char* input);
 int convertArgsToSeconds(char* args);
 int convertHoursMinsToSeconds(int hours, int minutes, int seconds);
 void convertIntToDoubleString(int number, char stringNumber[]);
 void printTimerEndTime (int seconds);
-void runEndSwitch(char* argSwitch, int *quietMode, int *dryRunMode, int *executeMode);
 void runProgram(char* programLocation, int silentOutput);
 int convert24ClockToSeconds(char* input);
 void checkFileExists(char* programLocation);
--- tests/args_switch_handler_test.c
+++ tests/args_switch_handler_test.c
@@ -1,24 +1,33 @@
 #include "../include/timer.h"
 #include "../include/testing.h"
 
-int quietMode = 0, dryRunMode = 0, executeMode = 0;
+int quiet_mode = 0, dry_run_mode = 0, execute_mode = 0;
 
 void checkQuietMode() {
     statement("Quiet Mode can be turned on");
-    runEndSwitch("--quiet", &quietMode, &dryRunMode, &executeMode);
-    assertInt(1, quietMode);
+    quiet_mode = 0;
+    dry_run_mode = 0;
+    execute_mode = 0;
+    t_set_mode_switch("--quiet", &quiet_mode, &dry_run_mode, &execute_mode);
+    assertInt(1, quiet_mode);
  }
 
 void checkDryRunMode() {
     statement("Dry run mode turned on with quiet mode as well");
-    runEndSwitch("--dry-run", &quietMode, &dryRunMode, &executeMode);
-    assertInt(2, quietMode + dryRunMode);
+    quiet_mode = 0;
+    dry_run_mode = 0;
+    execute_mode = 0;
+    t_set_mode_switch("--dry-run", &quiet_mode, &dry_run_mode, &execute_mode);
+    assertInt(2, quiet_mode + dry_run_mode);
  }
 
 void checkExecuteMode() {
     statement("Execute mode to be turned on with files with '.' in them");
-    runEndSwitch(".", &quietMode, &dryRunMode, &executeMode);
-    assertInt(2, quietMode + dryRunMode);
+    quiet_mode = 0;
+    dry_run_mode = 0;
+    execute_mode = 0;
+    t_set_mode_switch(".", &quiet_mode, &dry_run_mode, &execute_mode);
+    assertInt(2, quiet_mode + execute_mode);
  }
 
 void run_tests() {

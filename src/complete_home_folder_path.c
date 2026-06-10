3efd09e07803e40214c8ec191177396ee269f74e
Jason Keane <jason@random.com.au>
2026-06-10 22:26:15 +1000

    refactored complete home folder

--- src/complete_home_folder_path.c
+++ src/complete_home_folder_path.c
@@ -4,39 +4,40 @@
 #include <string.h>
 #define MAX_COMMAND_LENGTH 1024
 
-void completeHomeFolderPath(char* filePath){
-  if (filePath[0] == '~') {
-    char full_filename_path[MAX_COMMAND_LENGTH];
-    size_t home_length, remaining;
+void complete_home_folder_path(char* file_path){
+  if (file_path[0] != '~') {
+    return;
+  }
+
+  const char *home = NULL;
 
 #if defined(__linux__) || defined(__APPLE__)
-    char *home = getenv("HOME");
-    if (home == NULL) {
-      fprintf(stderr, "Error, HOME environment variable is not set\n");
-      exit(EXIT_FAILURE);
-    }
-    snprintf(full_filename_path, MAX_COMMAND_LENGTH, "%s", home);
+  home = getenv("HOME");
 #elif defined(_WIN32)
-    char *homedrive = getenv("HOMEDRIVE");
-    char *homepath = getenv("HOMEPATH");
-    if (homedrive == NULL || homepath == NULL) {
-      fprintf(stderr, "Error, HOMEDRIVE or HOMEPATH environment variable is"
-                      "not set");
-      exit(EXIT_FAILURE);
-    }
-    snprintf(full_filename_path, MAX_COMMAND_LENGTH, "%s%s", homedrive,
-             homepath);
-#endif
-    home_length = strlen(full_filename_path);
-    remaining = MAX_COMMAND_LENGTH - home_length - 1;
+  char temp[MAX_COMMAND_LENGTH];
+  const char *home_drive = getenv("HOMEDRIVE");
+  const char *home_path = getenv("HOMEPATH");
 
-    if (remaining <= 0) {
-      fprintf(stderr, "Error: Path too long, more than %d bytes", MAX_COMMAND_LENGTH);
-      exit(EXIT_FAILURE);
-    }
-
-    strncat(full_filename_path, filePath + 1, remaining);
-    strncpy(filePath, full_filename_path, MAX_COMMAND_LENGTH - 1);
-    filePath[MAX_COMMAND_LENGTH - 1] = '\0';
+  if (home_drive == NULL || home_path == NULL) {
+    fprintf(stderr, "Error: HOMEDRIVE or HOMEPATH environment variable not set\n");
+    exit(EXIT_FAILURE);
   }
+  snprintf(temp, MAX_COMMAND_LENGTH, "%s%s", home_drive, home_path);
+  home = temp;
+#endif
+
+  if (home == NULL){
+    fprintf(stderr, "Error: HOME environment variable not set\n");
+    exit(EXIT_FAILURE);
+  }
+
+  size_t home_length = strlen(home);
+  size_t suffix_length = strlen(file_path + 1);
+
+  if (home_length + suffix_length + 1 > MAX_COMMAND_LENGTH) {
+    fprintf(stderr, "Error: Path too long, more than %d bytes\n", MAX_COMMAND_LENGTH);
+    exit(EXIT_FAILURE);
+  }
+
+  snprintf(file_path, MAX_COMMAND_LENGTH, "%s%s", home, file_path + 1);
 }

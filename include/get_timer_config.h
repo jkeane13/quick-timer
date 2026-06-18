31368c18d65a03e0c2a71c95e204645c8e1a64b4
Jason Keane <jason@random.com.au>
2026-06-18 19:45:56 +1000

    Refactored get config file and move funciton to timer header

--- include/get_timer_config.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIMER_CONFIG_H
-#define TIMER_CONFIG_H
-
-void getConfigFromfileFile(char* filename,
-                           char* soundFile,
-                           char* alertTimes);
-#endif

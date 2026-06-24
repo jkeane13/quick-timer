ccdfbcd860d97a1e026b4e4057a81fa4fb100b0a
Jason Keane <jason@random.com.au>
2026-06-25 09:59:00 +1000

    Fixed risk of buffer overflow in main.c, by adding validation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "../include/complete_home_folder_path.h"
 #include "../include/timer.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "../include/play_audio.h"
 
@@ -30,6 +31,11 @@ int main(int argc, char *argv[]) {
   }
 
   if (strstr(argv[argc - 1], "--") != 0 || strstr(argv[argc - 1], ".") != 0) {
+    size_t arg_len = strlen(argv[argc - 1]);
+    if (arg_len >= MAX_STRING) {
+      fprintf(stderr, "Error: argument too long (max %d bytes)\n", MAX_STRING - 1);
+      return 1;
+    }
     strncpy(arg_switch, argv[argc - 1], MAX_STRING - 1);
     arg_switch[MAX_STRING - 1] = '\0';
     t_set_mode_switch(arg_switch, &quiet_mode, &dry_run_mode, &program_mode);
@@ -42,8 +48,15 @@ int main(int argc, char *argv[]) {
   check_file_exists(sound_file_path);
 
   for (int i = 1; i < argc; i++) {
-    strncat(time_string, argv[i], MAX_STRING - strlen(time_string) - 1);
-    strncat(time_string, " ", MAX_STRING - strlen(time_string) - 1);
+    size_t remaining = MAX_STRING - strlen(time_string) - 1;
+    size_t arg_len = strlen(argv[i]);
+    if (arg_len >= remaining) {
+      fprintf(stderr, "Error: arguments too long (max %d bytes total)\n", MAX_STRING - 1);
+      return 1;
+    }
+    strncat(time_string, argv[i], remaining);
+    if (i < argc - 1)
+      strncat(time_string, " ", MAX_STRING - strlen(time_string) - 1);
   }
   seconds = t_convert_args_to_seconds(time_string);
   t_print_timer_end_time(seconds);

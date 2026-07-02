44d62a7fa00b3210fee3a3005851c314ce6c8bbd
Jason Keane <jason@random.com.au>
2026-07-02 14:45:04 +1000

    Set system beep as a macro

--- src/play_audio.c
+++ src/play_audio.c
@@ -3,12 +3,14 @@
 #include <string.h>
 #include "../include/stopwatch_audio.h"
 
+#define SYSTEM_BEEP "\a"
+
 void play_sound(unsigned int times) {
   for (unsigned int i = 0; i < times; i++) {
     FILE *pipe = popen("ffplay -nodisp -autoexit - 2>/dev/null", "w");
     if (!pipe) {
       fprintf(stderr, "Warning: ffplay not found or installed, using system beep instead\n");
-      fprintf(stderr, "\a");
+      fprintf(stderr, SYSTEM_BEEP);
       continue;
     }
 
